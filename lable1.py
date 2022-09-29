0ccb87cbe0d4a30499120b025b89e25a30c4adc3
Blake <blake_gray1@baylor.edu>
2022-09-29 13:56:43 -0500

    Classwork 9/29/22

---
+++ lable1.py
@@ -0,0 +1,23 @@
+import tkinter
+
+
+
+class MyGUI:
+
+    def __init__(self):
+        self.main_window = tkinter.Tk()\
+        
+        self.main_window.geometry('720x720')
+        self.main_window.title('Label Demo')
+
+        self.Label1 = tkinter.Label(self.main_window,text='Hello World')
+        self.Label2 = tkinter.Label(self.main_window,text='This is my GUI program')
+        
+        self.Label1.pack()
+        self.Label2.pack()
+
+        tkinter.mainloop()
+
+MyGUI = MyGUI()
+
+print('Hi there!')
